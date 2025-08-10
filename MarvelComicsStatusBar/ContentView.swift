761f1918c7d63387fb5c33e3b010b0f0235e1b89
Reinaldo Arias <rariasp@ext.falabella.cl>
2025-08-10 15:12:02 -0400

    init files

---
+++ MarvelComicsStatusBar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MarvelComicsStatusBar
+//
+//  Created by Reinaldo Arias on 10/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MarvelComicsStatusBar/MarvelComicsStatusBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelComicsStatusBarApp.swift
+//  MarvelComicsStatusBar
+//
+//  Created by Reinaldo Arias on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MarvelComicsStatusBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
